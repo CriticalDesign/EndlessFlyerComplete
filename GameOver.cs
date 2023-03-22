3b14c77a1012ef832aaa087a928768af7a66d13a
CriticalDesign <100966505+CriticalDesign@users.noreply.github.com>
2023-03-22 10:25:21 -0400

    Add project files.

---
+++ GameOver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using static System.Formats.Asn1.AsnWriter;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GME1011EndlessFlyerComplete
+{
+    internal class GameOver
+    {
+        private Texture2D _gameOverTexture;
+
+        public GameOver(Texture2D gameOverTexture)
+        {
+            _gameOverTexture = gameOverTexture;
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+
+            spritebatch.Begin();
+            spritebatch.Draw(_gameOverTexture, new Vector2(600-250, 300-252), Color.White);
+            spritebatch.End();
+        }
+    }
+}
